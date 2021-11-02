7dfcbb1a5f56a47806b90f25639a813d73cd6f80
Ironmike84 <87212248+Ironmike84@users.noreply.github.com>
2021-11-02 13:03:02 -0400

    New Update 11/02/21
    New Changes to Files

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+pokemonList = {
+  //  Character 1 //    
+    name: "Jolteon",
+    height: 0.8,
+    weight:
+    24.5,
+    types: "electric",
+},
+  //  Character 2 //
+{    name: "Flareon",
+    height: 0.9,
+    weight: 25,
+    types: "fire", 
+},
+  //  Character 3 //
+{
+    name: "Espeon",
+    height: 0.9,
+    weight: 26.5,
+    types: "psychic"
+}
+
+document.write(`Name:${pokemonList.name} <br> Height: ${pokemonList.height} <br> Weight: ${pokemonList.weight} <br> Type:${pokemonList.types} `);
+
+
+
+
