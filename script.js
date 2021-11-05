b11dbff0fe74ae0dcbcd9624bd1a3483bb93d993
Ironmike84 <87212248+Ironmike84@users.noreply.github.com>
2021-11-05 17:15:40 -0400

    New Script
    Added New Script File

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
-<<<<<<< HEAD
 
-
+//Pokemon List Obejct Array 
 pokemonList = [
-  //  Character 1 //    
-    {name = "Jolteon",
-    height = 0.8,
-    weight = 24.5,
-    types = "electric"}
+    {name: "Jolteon",
+    height: 0.8,
+    weight: 24.5,
+    types: "electric"}
 ,
-  //  Character 2 //
 {    name: "Flareon",
     height: 0.9,
     weight: 25,
     types: "fire", },
-  //  Character 3 //
 {
     name: "Espeon",
     height: 0.9,
@@ -21,11 +17,41 @@ pokemonList = [
     types: "psychic"
 }
 ]
-document.write(`Name:${pokemonList.name} <br> Height: ${pokemonList.height} <br> Weight: ${pokemonList.weight} <br> Type:${pokemonList.types} `);
+
+//loop that iterates over the Pokemon List Array//
+
+for (let i = 0; i < pokemonList.length; i++) {
+  const elementEd = pokemonList[i];
+  const elementEdd = pokemonList[i].height;  
+console.log(elementEd, elementEdd)
+
+}
+// An Attempted Script to get the If Condtional Statement to work but it does not work...
+
+let user = prompt("Type in a Pokemon: Jolteon, Flareon, or Espeon...")
+function doThis(){
+  
+    if (user === "Jolteon" || "jolteon"){
+ console.log(pokemonList[0]);
+  }
+  else if (user === "Flareon" || "flareon"){
+    console.log(pokemonList[1]);
+  }
+  else if (user === "Epseon"){
+    console.log(pokemonList[2]);
+  }
+  else{
+    console.log("Do nothing");
+  }
+}
+doThis()
 
 
+ 
 
+// Listing All pokemon Character descriptions in a list on the left hand side of the page://
+document.write(`Name:${pokemonList[0].name} <br> Height: ${pokemonList[0].height} <br> Weight: ${pokemonList[0].weight} <br> Type:${pokemonList[1].types} <br>--------------<br>`);
 
-=======
+document.write(`Name:${pokemonList[1].name} <br> Height: ${pokemonList[1].height} <br> Weight: ${pokemonList[1].weight} <br> Type:${pokemonList[1].types} <br>--------------<br>`);
 
->>>>>>> 9c04fab35b6bbc84254ec7c24d3205ee6063b65c
+document.write(`Name:${pokemonList[2].name} <br> Height: ${pokemonList[2].height} <br> Weight: ${pokemonList[2].weight} <br> Type:${pokemonList[1].types} <br>--------------<br>`);
