e56ff08e83570ac5b058622abdbcab9650e1c353
Ironmike84 <87212248+Ironmike84@users.noreply.github.com>
2021-11-09 08:04:25 -0500

    Updated Script
    Updated Script File Including If Conditional

--- script.js
+++ script.js
@@ -19,41 +19,25 @@ let pokemonList = [
 ]
 
 //loop that iterates over the Pokemon List Array//
-for (let i = 0; i < pokemonList.length; i++) { 
-  document.write(pokemonList[i].name)
-  }
+// for (let i = 0; i < pokemonList.length; i++) { 
+//   document.write(pokemonList[i].name)
+//   }
 for (let i = 0; i < pokemonList.length; i++) {
   const elementEd = pokemonList[i];
   const elementEdd = pokemonList[i].height;  
 console.log(elementEd, elementEdd)
 
 }
-// An Attempted Script to get the If Condtional Statement to work but it does not work...
 
-let user = prompt("Type in a Pokemon: Jolteon, Flareon, or Espeon...")
-function doThis(){
-  
-    if (user.includes === "Jolteon" || "jolteon"){
- console.log(pokemonList[0]);
-  }
-  else if (user.includes === "Flareon" || "flareon"){
-    console.log(pokemonList[1]);
-  }
-  else if (user.includes = "Epseon"){
-    console.log(pokemonList[2]);
-  }
-  else{
-    console.log("Do nothing");
-  }
+//Conditional Stament For Writing Out The Biggest Pokemon Onto the DOM.
+let greatestWeight = 26.5
+if (pokemonList[0].weight === greatestWeight){
+  document.write(`${pokemonList[0].weight} lbs Wow, That's Big! ${pokemonList[0].name}`)
+}else if (pokemonList[1].weight === greatestWeight){
+  document.write(`${pokemonList[1].weight} lbs Wow, That's Big! ${pokemonList[1].name}`)
+}else if (pokemonList[2].weight === greatestWeight){
+  document.write(`${pokemonList[2].weight} lbs Wow, That's Big! ${pokemonList[2].name}`)
+}else{
+  console.log("No Pokemon Found...")
 }
-doThis()
-
-
  
-
-// Listing All pokemon Character descriptions in a list on the left hand side of the page://
-document.write(`Name:${pokemonList[0].name} <br> Height: ${pokemonList[0].height} <br> Weight: ${pokemonList[0].weight} <br> Type:${pokemonList[1].types} <br>--------------<br>`);
-
-document.write(`Name:${pokemonList[1].name} <br> Height: ${pokemonList[1].height} <br> Weight: ${pokemonList[1].weight} <br> Type:${pokemonList[1].types} <br>--------------<br>`);
-
-document.write(`Name:${pokemonList[2].name} <br> Height: ${pokemonList[2].height} <br> Weight: ${pokemonList[2].weight} <br> Type:${pokemonList[1].types} <br>--------------<br>`);
