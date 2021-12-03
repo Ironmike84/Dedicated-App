84d42c7c9463c75b289e955cb610cd649325bb4c
Ironmike84 <87212248+Ironmike84@users.noreply.github.com>
2021-12-03 16:35:35 -0500

    Modal Script
    ModalScript

---
+++ Modal Script.js
@@ -0,0 +1,21 @@
+
+$('#closeButton').click(function(){
+    let modal = $('#exampleModalCenter')
+    $(modal).css('opacity','.01' )
+})
+
+$('#closeOut').click(function(){
+    let modal = $('#exampleModalCenter')
+    $(modal).css('opacity','.01' )
+})
+
+$(window).keypress(function(){
+    let modal = $('#exampleModalCenter')
+    $(modal).css('opacity','.01' )
+})
+
+
+$(window).click(function(){
+    let modal = $('#exampleModalCenter')
+    $(modal).css('opacity','.01' )
+})
