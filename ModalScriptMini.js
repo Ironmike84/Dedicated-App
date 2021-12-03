ee57be468ecb397bb73aee0bf0b591bfa6aef81c
Ironmike84 <87212248+Ironmike84@users.noreply.github.com>
2021-12-03 17:18:11 -0500

    Minified Modal Script

---
+++ ModalScriptMini.js
@@ -0,0 +1,1 @@
+$("#closeButton").click(function(){let e=$("#exampleModalCenter");$(e).css("opacity",".01")}),$("#closeOut").click(function(){let e=$("#exampleModalCenter");$(e).css("opacity",".01")}),$(window).keypress(function(){let e=$("#exampleModalCenter");$(e).css("opacity",".01")}),$(window).click(function(){let e=$("#exampleModalCenter");$(e).css("opacity",".01")});
